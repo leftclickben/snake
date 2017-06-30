595401373937ef3ef3d4f9d96e2b7dc139bcb8a8
Ben New <ben@leftclick.com.au>
2017-07-01 00:43:49 +0800

    Add `Direction.fromMovement()` and fix `Direction.random()`.

--- public/app/util/Direction.js
+++ public/app/util/Direction.js
@@ -9,4 +9,9 @@ Direction.UP = 38;
 Direction.RIGHT = 39;
 Direction.DOWN = 40;
 
-Direction.random = () => Direction[Random.key(Direction)];
+Direction.random = () => 37 + Random.integer(4);
+
+Direction.fromMovement = (dx, dy) =>
+    (Math.abs(dx) > Math.abs(dy)) ?
+        (dx < 0 ? Direction.LEFT : Direction.RIGHT) :
+        (dy < 0 ? Direction.UP : Direction.DOWN);
--- test/spec/util/Direction.spec.js
+++ test/spec/util/Direction.spec.js
@@ -13,10 +13,53 @@ describe('The `Direction` utility module', () => {
     });
     it('Exposes the correct static methods', () => {
         expect(Direction.random).to.be.a('function');
+        expect(Direction.fromMovement).to.be.a('function');
     });
     describe('The `random` static method', () => {
         it('Returns one of the constants', () => {
             expect([ Direction.LEFT, Direction.UP, Direction.RIGHT, Direction.DOWN ]).to.contain(Direction.random());
         })
+    });
+    describe('The `fromMovement` static method', () => {
+        describe('When moving directly left', () => {
+            it('Returns left', () => {
+                expect(Direction.fromMovement(-1, 0)).to.equal(Direction.LEFT);
+            });
+        });
+        describe('When moving approximately left', () => {
+            it('Returns left', () => {
+                expect(Direction.fromMovement(-10, 6)).to.equal(Direction.LEFT);
+            });
+        });
+        describe('When moving directly up', () => {
+            it('Returns up', () => {
+                expect(Direction.fromMovement(0, -1)).to.equal(Direction.UP);
+            });
+        });
+        describe('When moving approximately up', () => {
+            it('Returns up', () => {
+                expect(Direction.fromMovement(6, -10)).to.equal(Direction.UP);
+            });
+        });
+        describe('When moving directly right', () => {
+            it('Returns right', () => {
+                expect(Direction.fromMovement(1, 0)).to.equal(Direction.RIGHT);
+            });
+        });
+        describe('When moving approximately right', () => {
+            it('Returns right', () => {
+                expect(Direction.fromMovement(10, 6)).to.equal(Direction.RIGHT);
+            });
+        });
+        describe('When moving directly down', () => {
+            it('Returns down', () => {
+                expect(Direction.fromMovement(0, 1)).to.equal(Direction.DOWN);
+            });
+        });
+        describe('When moving approximately down', () => {
+            it('Returns down', () => {
+                expect(Direction.fromMovement(6, 10)).to.equal(Direction.DOWN);
+            });
+        });
     });
 });
